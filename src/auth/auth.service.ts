76bc753a759a49c7a4450002c94ab22980fcaa7e
Syed Hasnain Mehadi <hasnain.mehadi@neosoftmail.com>
2024-12-23 22:35:24 +0530

    worked on password bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,18 @@ import { CreateUserDto } from './auth.dto';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Users } from '../users/users.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(Users.name) private catModel: Model<Users>) {}
 
   async signup(createUser: CreateUserDto): Promise<Users> {
-    const createdCat = new this.catModel(createUser);
+    const hash = await bcrypt.hash(
+      createUser.password,
+      Number(process.env.PASSWORD_ENCRYPT_ROUND),
+    );
+    const createdCat = new this.catModel({ ...createUser, password: hash });
     return await createdCat.save();
   }
 
